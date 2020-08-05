6a4141a068c6a29ecd7a18d73c9d0ce4fbf2f052
MarioOcampo <62075013+MarioOcampo1@users.noreply.github.com>
2020-08-05 17:15:47 -0300

    Add files via upload

---
+++ Proyecto/Mario  Ocampo/3.0/Covid.py
@@ -0,0 +1,11 @@
+class Covid:
+    x = 1
+    y = 1
+    step = 99
+
+    def __init__(self, x, y):
+        self.x = x * self.step
+        self.y = y * self.step
+
+    def draw(self, surface, image):
+        surface.blit(image, (self.x, self.y))
