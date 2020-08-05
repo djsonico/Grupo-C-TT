6606cea48b539a5692d4756844174615a555c4b2
herreratomas <63070252+herreratomas@users.noreply.github.com>
2020-08-05 17:15:36 -0300

    Add files via upload

---
+++ Rec.Global_MDI_clase_Juego_Main/Main.py
@@ -0,0 +1,11 @@
+from Juego import *
+from Aplicacion import *
+from Covid import *
+from Jugador import *
+from Computadora import*
+
+# Recuerda tener todos los archivos en una misma carpeta
+# para que los imports no tengan problemas.
+if __name__ == '__main__':
+    aplicacion = Aplicacion()
+    aplicacion.on_execute()
